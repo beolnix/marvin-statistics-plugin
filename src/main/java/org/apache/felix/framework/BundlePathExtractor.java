855e9b8cc84c6e10fc4ba7dd944b4504ed6003e1
beolnix <beolnix@gmail.com>
2016-11-06 19:14:40 +0100

    add comments for spring hacks

--- src/main/java/org/apache/felix/framework/BundlePathExtractor.java
+++ src/main/java/org/apache/felix/framework/BundlePathExtractor.java
@@ -1,5 +1,6 @@
 package org.apache.felix.framework;
 
+import com.google.gag.annotation.remark.Hack;
 import org.osgi.framework.Bundle;
 
 import java.net.URL;
@@ -7,6 +8,10 @@ import java.net.URL;
 /**
  * Created by beolnix on 11/02/16.
  */
+
+// this hack is needed to let spring component scan work in OSGi env.
+
+@Hack
 public class BundlePathExtractor {
 
     public static String getBundlePath(Bundle bundle, URL url) throws Exception {
--- src/main/java/org/eclipse/core/runtime/FileLocator.java
+++ src/main/java/org/eclipse/core/runtime/FileLocator.java
@@ -1,12 +1,18 @@
 package org.eclipse.core.runtime;
 
-import java.net.URL;
-
-import org.apache.felix.framework.BundlePathExtractor;
+import com.google.gag.annotation.remark.Hack;
 import org.osgi.framework.Bundle;
-
 import org.osgi.framework.FrameworkUtil;
 
+import java.net.URL;
+
+// this hack is needed to let spring component scan work in OSGi env.
+// spring literally checks if there is a org.eclipse.core.runtime.FileLocator class in the
+// class path and uses it if it does exist
+//
+// https://github.com/spring-projects/spring-framework/blob/bc14c5ba83e1f211628456bbccce7b2531aac58c/spring-core/src/main/java/org/springframework/core/io/support/PathMatchingResourcePatternResolver.java#L189
+//
+@Hack
 public class FileLocator {
     private FileLocator() {}
 
